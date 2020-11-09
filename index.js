f1565c7d64be18564a1481e388198695ced8345d
softwaree40 <softwaree40@gmail.com>
2020-11-08 20:29:33 -0500

    adding overlay also hide overlay

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 "use strict"
-let wind = document.querySelectorAll(".wind")
-wind.forEach(element =>{
-element.addEventListener("click",function(){
-    console.warn("I am click")
-})
- 
-})
+let windowMy = document.querySelector(".windows")
+let closeWindowBtn = document.querySelector(".close-window")
+let overlay = document.querySelector(".overlay")
+let Allwind = document.querySelectorAll(".wind")
+//window to pop also with overlay
+const openWindow = function(){
 
+  windowMy.classList.remove("hidden")
+  overlay.classList.remove("hidden")
+}
+Allwind.forEach(wind =>{wind.addEventListener("click",openWindow)})
+
+const closeWindow = function(){
+
+  windowMy.classList.add("hidden");
+  overlay.classList.add("hidden");
+
+}
+//Close window and hide overlay
+closeWindowBtn.addEventListener("click",closeWindow)
+
+
+overlay.addEventListener("click",closeWindow)
